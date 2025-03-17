906e4c16f0b656c345308ff67fdfb6c0f480e443
Luis Antonio Garcia Roque <78177901+toonymak1993@users.noreply.github.com>
2025-03-17 09:54:36 +0100

    audio_device_ui

--- GAMINGCONSOLEMODE/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/GAMINGCONSOLEMODE.AssemblyInfo.cs
+++ GAMINGCONSOLEMODE/obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/GAMINGCONSOLEMODE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GAMINGCONSOLEMODE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86a58acd9c97936fffef93e67af076ebd12f3346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e3589c8b151acbb02c6e0ab12b3281c93ce0fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("GAMINGCONSOLEMODE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GAMINGCONSOLEMODE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
